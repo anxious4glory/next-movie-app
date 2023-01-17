810f8bdb9ff66f77af93371d88c00015644347f6
anxious4glory <53601508+anxious4glory@users.noreply.github.com>
2023-01-17 02:32:38 +0100

    first commit

---
+++ component/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <div className="text-center h-32 flex items-center justify-center">
+        <p className="text-xl text-gray-800">&copy; Copyright 2023 <span className="font-bold">CYBER MAVEN</span></p>
+      </div>
+    );
+  };
+  
+  export default Footer;
